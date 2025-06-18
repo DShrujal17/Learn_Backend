8b2c6338a17e8017cb9922fa734902f9d3436431
shrujal <shrujaldoshi907@gmail.com>
2025-06-18 13:19:57 +0530

    add changes

--- EJS/index.js
+++ EJS/index.js
@@ -93,26 +93,26 @@
 
 // Example 5 : Instagram page with EJS
 
-const express = require("express");
-const app = express();
-const port = 8080;
-const path = require("path");
+// const express = require("express");
+// const app = express();
+// const port = 8080;
+// const path = require("path");
 
-app.use(express.static(path.join(__dirname,"public")));
-app.set("view engine","ejs");
-app.set("views",path.join(__dirname,"views"));
+// app.use(express.static(path.join(__dirname,"public")));
+// app.set("view engine","ejs");
+// app.set("views",path.join(__dirname,"views"));
 
-app.get("/ig/:username",(req,res) => {
-    let {username} = req.params;
-    const instaData = require("./data.json")
-    const data = instaData[username];
-    console.log(data);
-    res.render("instagram2.ejs", { data });
-});
+// app.get("/ig/:username",(req,res) => {
+//     let {username} = req.params;
+//     const instaData = require("./data.json")
+//     const data = instaData[username];
+//     console.log(data);
+//     res.render("instagram2.ejs", { data });
+// });
 
-app.listen(port,() => {
-    console.log(`listening on port ${port}`)
-});
+// app.listen(port,() => {
+//     console.log(`listening on port ${port}`)
+// });
 
 
 
